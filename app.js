bf4cd6ea78d6a727908f15d7b55f6a816fbad37d
hangyu cho <chg3376@gmail.com>
2019-01-18 22:57:46 +0900

    modify rest

--- app.js
+++ app.js
@@ -4,20 +4,9 @@ var app = express()
 const request = require('request');
 const url = 'https://www.naver.com';
 
-app.get('/',function(req,res){
-    //res.send('hello world!');
+app.get('/hello',function(req,res){
+    res.send('hello world!');
 
-    request({
-        uri: url
-        ,method: 'GET'
-        ,headers: {
-            'Accept-Charset':'utf-8'
-        }
-
-    },function(err,res,body){
-        console.log(body);
-        res.send(body);
-    });
 });
 app.listen(3000,function(){
     console.log('im 3000 port!!!')
