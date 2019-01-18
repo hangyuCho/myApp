b6821fdccdbf9824ff7be703c0a2feddbdd710b1
hangyu cho <chg3376@gmail.com>
2019-01-18 23:03:57 +0900

    modify port for heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express')
-var app = express()
-
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT;
 const request = require('request');
 const url = 'https://www.naver.com';
 
@@ -8,6 +8,6 @@ app.get('/hello',function(req,res){
     res.send('hello world!');
 
 });
-app.listen(3000,function(){
-    console.log('im 3000 port!!!')
+app.listen(PORT,function(){
+    console.log('im '+PORT+' port!!!')
 });
